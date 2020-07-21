50069574917021861a8d706132b7783bdeaf33a2
Antonio James <asjames18@gmail.com>
2020-07-21 17:06:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,6 @@
 class Dog
-  def initialize(breed)
-    @breed = breed
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
+  def initialize(name, breed)
+    @name = name
   end
 end
+
