92adb30194ee466e56d642dca82cfe372105b59d
Antonio James <asjames18@gmail.com>
2020-07-21 16:53:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,12 +2,10 @@ class Person
   def initialize(name)
     @name = name
   end
-  def name(name)
-    @name = name
-  end
-  
+  # def name(name)
+  #   @name = name
+  # end
+
 end
 
 beyonce = Person.new("Beyonce")
-
-beyonce.name
